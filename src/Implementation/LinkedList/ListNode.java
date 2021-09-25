5d51752c14e420cc82e44b3f14eb3506e72b7e3d
Vijay Singh <dprophecyguy@gmail.com>
2021-09-26 03:08:44 +0530

    DAY-5 Fixed Syntax issue in ListNode.java

--- src/Implementation/LinkedList/ListNode.java
+++ src/Implementation/LinkedList/ListNode.java
@@ -6,7 +6,7 @@ public class ListNode {
     public ListNode next;
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
-    public ListNode(int val, ListNode next) { this.val = vgal; this.next = next; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
     @Override
     public String toString() {
